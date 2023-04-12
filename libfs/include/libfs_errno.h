f5a6fc73f1eea1741cb9c6f6b8eb6801b1a1d0d0
Stanisław Zagórowski <hasohasacz@gmail.com>
2023-04-12 19:05:48 +0200

    Make libfs_errno thread-local

--- libfs/include/libfs_errno.h
+++ libfs/include/libfs_errno.h
@@ -1,10 +1,11 @@
 #pragma once
+#include <threads.h>
 
 // Error codes
 #define LIBFS_ERRNO_OK 0
 #define LIBFS_ERRNO_UNKNOWN -1
 
-extern int libfs_errno;
+extern thread_local int libfs_errno;
 
 void libfs_set_errno(int errno);
 
