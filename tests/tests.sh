9e1ec4c0c908cb8e93a84e7dc436b548e1f60c94
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-06-02 17:46:18 +0200

    Update tests to be more appropriate

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,10 +1,12 @@
-../clients/libfs-create/libfs-create hello.txt
-fdw=$(../clients/libfs-open/libfs-open -w hello.txt)
+set -euox
+
+fdw=$(../clients/libfs-create/libfs-create hello.txt)
+
 ../clients/libfs-write/libfs-write $fdw "Hello"
 ../clients/libfs-close/libfs-close $fdw
 
 fdr=$(../clients/libfs-open/libfs-open -r hello.txt)
-content=$(../clients/libfs/libfs -read $fdr)
+content=$(../clients/libfs-read/libfs-read $fdr)
 ../clients/libfs-close/libfs-close $fdr
 
 echo $content
