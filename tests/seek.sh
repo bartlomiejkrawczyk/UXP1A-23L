0457d194fcda4e382eb8c8c413d644c5b27d90f2
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-06-07 14:07:24 +0200

    Add seek tests

--- tests/seek.sh
+++ tests/seek.sh
@@ -1,12 +1,45 @@
 #!/bin/bash
 
-FDW=$(libfs-create test.txt)
-BYTES_WRITTEN=$(libfs-write $FDW "Before seek")
-libfs-close $FDW
+FILE="seek.txt"
 
-FDR=$(libfs-open test.txt -r)
-libfs-seek $FDR 7
-CONTENT=$(libfs-read $FDR 1024)
-libfs-close $FDR
+# Prepare files
 
-echo $CONTENT
+FDRW=$(libfs-create "$FILE" rw-rw-rw-)
+BYTES_WRITTEN=$(libfs-write "$FDRW" "1")
+libfs-close "$FDRW"
+
+FDRW=$(libfs-open "$FILE")
+
+libfs-seek "$FDRW" 1
+
+BYTES_WRITTEN=$(libfs-write "$FDRW" "2")
+
+BYTES_WRITTEN=$(libfs-write "$FDRW" "3")
+
+libfs-seek "$FDRW" -3
+
+CONTENT=$(libfs-read "$FDRW" 1024)
+echo "$CONTENT"
+
+libfs-seek "$FDRW" -2
+
+BYTES_WRITTEN=$(libfs-write "$FDRW" "0")
+
+libfs-seek "$FDRW" 1
+
+BYTES_WRITTEN=$(libfs-write "$FDRW" "4")
+
+libfs-seek "$FDRW" -4
+
+CONTENT=$(libfs-read "$FDRW" 1024)
+echo "$CONTENT"
+
+# Seek out of file range
+
+libfs-seek "$FDRW" -10
+
+libfs-close "$FDRW"
+
+# Clean up
+
+libfs-unlink "$FILE"
--- tests/seek.spec
+++ tests/seek.spec
@@ -1,2 +1,4 @@
 0
-seek
+123
+1034
+[31;1m[ERROR] [0mlibfs_seek failed: Invalid argument (src/main.c:27)
