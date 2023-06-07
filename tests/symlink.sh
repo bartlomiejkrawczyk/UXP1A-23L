11a06ac0baa14530cccd0a2b62ba4a6ce14ba1d3
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-06-07 14:17:31 +0200

    Write symlink tests

--- tests/symlink.sh
+++ tests/symlink.sh
@@ -1,13 +1,59 @@
 #!/bin/bash
 
-FDW=$(libfs-create test_symlink.txt)
-BYTES_WRITTEN=$(libfs-write $FDW "Before symlink")
-libfs-close $FDW
+FILE="link.txt"
+SYMLINK="symlink.txt"
 
-libfs-symlink test_symlink.txt test_symlink2.txt
+libfs_write() {
+    FDW=$(libfs-open "$1" -w)
+    BYTES_WRITTEN=$(libfs-write "$FDW" "$2")
+    libfs-close "$FDW"
+}
 
-FDR=$(libfs-open test_symlink2.txt -r)
-CONTENT=$(libfs-read $FDR 1024)
-libfs-close $FDR
+libfs_read() {
+    FDR=$(libfs-open "$1" -r)
+    CONTENT=$(libfs-read "$FDR" 1024)
+    libfs-close "$FDR"
 
-echo $CONTENT
+    echo $CONTENT
+}
+
+# Prepare files
+
+FDW=$(libfs-create "$FILE" rwxrwxrwx)
+BYTES_WRITTEN=$(libfs-write "$FDW" "BEFORE")
+libfs-close "$FDW"
+
+# Create symlink between files
+
+libfs-link "$FILE" "$SYMLINK"
+
+# Read and write is possible using symlink
+
+libfs_read "$SYMLINK"
+
+libfs_write "$SYMLINK" "LINK  "
+
+# Changes are made to the FILE too
+
+libfs_read "$FILE"
+
+# Delete file pointed by symlink
+
+libfs-unlink "$FILE"
+
+# File content is not available
+
+libfs_read "$SYMLINK"
+
+FDW=$(libfs-create "$FILE" rwxrwxrwx)
+BYTES_WRITTEN=$(libfs-write "$FDW" "AFTER")
+libfs-close "$FDW"
+
+# Now the content is available again
+
+libfs_read "$SYMLINK"
+
+# Clean up
+
+libfs-unlink "$FILE"
+libfs-unlink "$SYMLINK"
--- tests/symlink.spec
+++ tests/symlink.spec
@@ -1,2 +1,5 @@
 0
-Before symlink
+BEFORE
+LINK
+[31;1m[ERROR] [0mlibfs_open failed: No such file or directory (src/main.c:38)
+AFTER
